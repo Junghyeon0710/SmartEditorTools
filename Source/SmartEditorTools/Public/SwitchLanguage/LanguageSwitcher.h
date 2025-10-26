3f49312523fb86942410a58a8e17b82039a2806f
Junghyeon0710 <sie08357@naver.com>
2025-10-26 17:21:36 +0900

    feat: 언어 변환기 클래스 추가

---
+++ Source/SmartEditorTools/Public/SwitchLanguage/LanguageSwitcher.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "LanguageSwitcher.generated.h"
+
+
+class FLanguageSwitcher 
+{
+	
+};
